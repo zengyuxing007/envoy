3fa160d54bbf7b768d51d25101635ed8d0c23ce5
zengyuxing <hzzengyuxing@corp.netease.com>
2019-05-29 14:40:23 +0800

    add some comment
    Signed-off-by: zengyuxing <hzzengyuxing@corp.netease.com>

--- examples/traffic_marking/start_service_iptables.sh
+++ examples/traffic_marking/start_service_iptables.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+### inbound 流量只劫持 9080 端口
+### outbound 劫持全部tcp 流量到18081 端口
+
 /usr/local/bin/istio-iptables.sh -p 18081 -u 1000  -m REDIRECT -i "*" -x ""   -b "" -d "9080"
 python3 /code/service.py &
 su - demo_user -s /bin/bash -c "envoy -c /etc/service-envoy.yaml --service-cluster service${SERVICE_NAME} -l trace"
